4ee9dab70ed4489e1a615016a0aeaf8279a264d1
Saman <samanezzatabadi@yahoo.com>
2025-12-27 11:06:37 +0330

    some fixin

--- src/common/middlewares/rateLimiter.js
+++ src/common/middlewares/rateLimiter.js
@@ -4,5 +4,10 @@ export const rateLimiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 2 minutes
   // windowMs: 10 * 1000, // 10 secs
   max: 2, // max 5 requests per IP
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req) => {
+    return req.ip; // IMPORTANT
+  },
   message: "Too many requests, Wait 2 minutes and try again.",
 });
